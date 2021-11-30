48ec1e1c7874d2a642b98de6dcbbe62f213cbaab
smyalygames <smyalygames@gmail.com>
2021-11-30 00:50:50 +0000

    Added a landing page informing that the website is under construction

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+	require 'header.php';
+?>
+
+<h1>Welcome to the TofuLinux Website!</h1>
+<h2>This website is currently under construction...</h2>
+<h2>In the meanwhile, you can take a look at the TofuLinux Discord Music Bot which will most likely be offline: <a href="https://discord.com/api/oauth2/authorize?client_id=886956739369521183&amp;permissions=3213312&amp;scope=bot">here</a></h2>
+
+<?php
+	require 'footer.php';
+?>
